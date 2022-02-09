6ef36f86418699d77aac81879903c143e651d3d0
fabi <fabi@soxbox.tk>
2022-02-09 23:09:14 +0200

    wip

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -40,86 +40,86 @@ def call(Map userConfigVariables=[:]) {
     //     ]
     // )
     podTemplate(yaml: '''
-    apiVersion: v1
-    kind: Pod
-    metadata:
-    name: build-docker-image
-    spec:
-    containers:
-    - name: gcloud
-        image: gcr.io/google.com/cloudsdktool/cloud-sdk:latest
-        command:
-        - cat
-        tty: true
-        volumeMounts:
-        - name: kaniko-secret
-            mountPath: /secret
-        env:
-        - name: GOOGLE_APPLICATION_CREDENTIALS
-            value: /secret/av-cms-293206-90534c67e5c6.json
+        apiVersion: v1
+        kind: Pod
+        metadata:
+        name: build-docker-image
+        spec:
+        containers:
+        - name: gcloud
+            image: gcr.io/google.com/cloudsdktool/cloud-sdk:latest
+            command:
+            - cat
+            tty: true
+            volumeMounts:
+            - name: kaniko-secret
+                mountPath: /secret
+            env:
+            - name: GOOGLE_APPLICATION_CREDENTIALS
+                value: /secret/av-cms-293206-90534c67e5c6.json
 
 
-    - name: kaniko-gcp
-        image: gcr.io/kaniko-project/executor:v1.6.0-debug
-        imagePullPolicy: Always
-        command:
-        - /busybox/cat
-        tty: true
-        volumeMounts:
-        - name: kaniko-secret
-            mountPath: /secret
-        - name: kaniko-cache
-            mountPath: /cache
+        - name: kaniko-gcp
+            image: gcr.io/kaniko-project/executor:v1.6.0-debug
+            imagePullPolicy: Always
+            command:
+            - /busybox/cat
+            tty: true
+            volumeMounts:
+            - name: kaniko-secret
+                mountPath: /secret
+            - name: kaniko-cache
+                mountPath: /cache
+                readOnly: true
+            env:
+            - name: GOOGLE_APPLICATION_CREDENTIALS
+                value: /secret/av-cms-293206-90534c67e5c6.json
+
+
+        - name: kaniko-aws
+            image: gcr.io/kaniko-project/executor:v1.6.0-debug
+            imagePullPolicy: Always
+            command:
+            - cat
+            tty: true
+            volumeMounts:
+            - name: docker-config
+                mountPath: /kaniko/.docker
+            - name: aws-secret
+                mountPath: /root/.aws/
+
+
+        - name: python
+            image: python:latest
+            imagePullPolicy: Always
+            command:
+            - cat
+            tty: true
+
+
+        volumes:
+            - name: docker-config
+            configMap:
+                name: docker-config
+            - name: aws-secret
+            secret:
+                secretName: aws-secret
+            - name: kaniko-secret
+            secret:
+                secretName: kaniko-secret
+            - name: kaniko-cache
             readOnly: true
-        env:
-        - name: GOOGLE_APPLICATION_CREDENTIALS
-            value: /secret/av-cms-293206-90534c67e5c6.json
+            persistentVolumeClaim:
+                claimName: cd-jenkins-agent-kaniko-cache
 
 
-    - name: kaniko-aws
-        image: gcr.io/kaniko-project/executor:v1.6.0-debug
-        imagePullPolicy: Always
-        command:
-        - cat
-        tty: true
-        volumeMounts:
-        - name: docker-config
-            mountPath: /kaniko/.docker
-        - name: aws-secret
-            mountPath: /root/.aws/
-
-
-    - name: python
-        image: python:latest
-        imagePullPolicy: Always
-        command:
-        - cat
-        tty: true
-
-
-    volumes:
-        - name: docker-config
-        configMap:
-            name: docker-config
-        - name: aws-secret
-        secret:
-            secretName: aws-secret
-        - name: kaniko-secret
-        secret:
-            secretName: kaniko-secret
-        - name: kaniko-cache
-        readOnly: true
-        persistentVolumeClaim:
-            claimName: cd-jenkins-agent-kaniko-cache
-
-
-    tolerations:
-    - effect: NoSchedule
-        key: tier
-        operator: Equal
-        value: cicd
-    nodeSelector:
-        tier: cicd
+        tolerations:
+        - effect: NoSchedule
+            key: tier
+            operator: Equal
+            value: cicd
+        nodeSelector:
+            tier: cicd
     ''')
     {
         node(POD_LABEL) {
