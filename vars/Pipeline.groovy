1a012424c64d9379806e3d7dd7f7b1b687641d86
fabi <fabi@soxbox.tk>
2022-02-09 23:07:58 +0200

    wip

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -11,18 +11,116 @@ import pipelines.Parameters
 def call(Map userConfigVariables=[:]) {
     def context = [:]
 
-    podTemplate(containers: [
-        containerTemplate(
-            name: 'jnlp', 
-            image: 'jenkins/inbound-agent:latest'
-            ),
-        containerTemplate(
-            name: 'python', 
-            image: 'python:latest', 
-            command: 'sleep', 
-            args: '30d'
-        )
-    ])
+    // podTemplate(
+    //     nodeSelector: 'tier: cicd',
+    //     containers: [
+    //         containerTemplate(
+    //             name: 'jnlp', 
+    //             image: 'jenkins/inbound-agent:latest'
+    //             ),
+    //         containerTemplate(
+    //             name: 'python', 
+    //             image: 'python:latest', 
+    //             command: 'sleep', 
+    //             args: '30d'
+    //         ),
+    //         containerTemplate(
+    //             name: 'gcloud', 
+    //             image: 'gcr.io/google.com/cloudsdktool/cloud-sdk:latest', 
+    //             command: 'cat',
+    //             ttyEnabled: true,
+    //             envVars: [
+    //                 envVar(key: 'GOOGLE_APPLICATION_CREDENTIALS', value: '/secret/av-cms-293206-90534c67e5c6.json')
+    //             ]
+    //         )
+    //     ],
+    //     volumes: [
+    //         secretVolume(secretName: 'kaniko-secret', mountPath: '/secret'),
+    //         persistentVolumeClaim(claimName: 'cd-jenkins-agent-kaniko-cache', mountPath: '/cache')
+    //     ]
+    // )
+    podTemplate(yaml: '''
+    apiVersion: v1
+    kind: Pod
+    metadata:
+    name: build-docker-image
+    spec:
+    containers:
+    - name: gcloud
+        image: gcr.io/google.com/cloudsdktool/cloud-sdk:latest
+        command:
+        - cat
+        tty: true
+        volumeMounts:
+        - name: kaniko-secret
+            mountPath: /secret
+        env:
+        - name: GOOGLE_APPLICATION_CREDENTIALS
+            value: /secret/av-cms-293206-90534c67e5c6.json
+
+
+    - name: kaniko-gcp
+        image: gcr.io/kaniko-project/executor:v1.6.0-debug
+        imagePullPolicy: Always
+        command:
+        - /busybox/cat
+        tty: true
+        volumeMounts:
+        - name: kaniko-secret
+            mountPath: /secret
+        - name: kaniko-cache
+            mountPath: /cache
+            readOnly: true
+        env:
+        - name: GOOGLE_APPLICATION_CREDENTIALS
+            value: /secret/av-cms-293206-90534c67e5c6.json
+
+
+    - name: kaniko-aws
+        image: gcr.io/kaniko-project/executor:v1.6.0-debug
+        imagePullPolicy: Always
+        command:
+        - cat
+        tty: true
+        volumeMounts:
+        - name: docker-config
+            mountPath: /kaniko/.docker
+        - name: aws-secret
+            mountPath: /root/.aws/
+
+
+    - name: python
+        image: python:latest
+        imagePullPolicy: Always
+        command:
+        - cat
+        tty: true
+
+
+    volumes:
+        - name: docker-config
+        configMap:
+            name: docker-config
+        - name: aws-secret
+        secret:
+            secretName: aws-secret
+        - name: kaniko-secret
+        secret:
+            secretName: kaniko-secret
+        - name: kaniko-cache
+        readOnly: true
+        persistentVolumeClaim:
+            claimName: cd-jenkins-agent-kaniko-cache
+
+
+    tolerations:
+    - effect: NoSchedule
+        key: tier
+        operator: Equal
+        value: cicd
+    nodeSelector:
+        tier: cicd
+    ''')
     {
         node(POD_LABEL) {
             stage("INIT") {
